fa9d20ea6c5867c7112f1fbf1c72e67b5a1b35f4
Francis Tseng <ftzeng@gmail.com>
2011-12-30 15:32:49 +0800

    textsplitter.rb now handles multiple texts

--- ruby/textsplitter.rb
+++ ruby/textsplitter.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-freqs = Hash.new(0)
+freqs = Hash.new { |hash,key| hash[key] = [] }
 filearray = Array[ "ruby/text.txt", "ruby/text2.txt", "ruby/text3.txt" ]
-filearray.each do |item|
+filearray.each_with_index do |item,index|
 	words = File.open(item) { |f| f.read }.downcase.split(/[^a-zA-Z]/)
-	words.each { |word| freqs[word] += 1 }
+	words.each { |word| freqs[word][index].nil? ? freqs[word] << 1 : freqs[word][index] += 1 }
 end
 puts freqs
